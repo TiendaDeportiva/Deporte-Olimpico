fabd0dcf43eb9c97090f302b7ab7dd9e817aa49f
Decoders <decoderscompany@gmail.com>
2022-08-22 20:11:06 -0500

    Commit Paula y Gonzalo creando archivos base

---
+++ src/main/java/com/tiendadeportiva/deporteolimpico/DeporteolimpicoApplication.java
@@ -0,0 +1,13 @@
+package com.tiendadeportiva.deporteolimpico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeporteolimpicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeporteolimpicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendadeportiva/deporteolimpico/DeporteolimpicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendadeportiva.deporteolimpico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeporteolimpicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
